96c2d39a8a1f4a00ec74f52e60025c353cffc577
dengxw <dengxw@rd.netease.com>
2022-03-29 22:46:35 +0800

    'init'

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.demo.controller;
+
+import com.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/account")
+public class DemoController {
+
+    @Autowired
+    private DemoService demoService;
+
+    @RequestMapping("get")
+    public String get(@RequestParam String id){
+        return "hello,"+id+"!";
+    }
+
+}
