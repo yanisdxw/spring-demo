ece18998be093768fb0c549b710b63454626e895
dengxw <dengxw@rd.netease.com>
2022-05-01 18:24:43 +0800

    remake project

--- spring-example/src/main/java/com/dxw/service/DemoService.java
+++ spring-example/src/main/java/com/dxw/service/DemoService.java
@@ -4,6 +4,9 @@ import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 @Service
 public class DemoService {
 
@@ -12,6 +15,13 @@ public class DemoService {
         for (int i = 0; i < 100; i++) {
             sseEmitter.send(SseEmitter.event().name(guid).data(i));
         }
+        int[][] a = new int[][]{{0,1}};
+        Arrays.sort(a, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]-o2[0];
+            }
+        });
     }
 
 }
